9a1f520ac218ed2a843270caa4c8974175d7c387
Raymond Augé <rotty3000@apache.org>
2021-06-27 11:22:22 -0400

    ARIES-2052 ProxyServices#supportsClassDefining() in weld 3.1.7 breaks aries-cdi
    Signed-off-by: Raymond Augé <rotty3000@apache.org>

--- cdi-weld/src/main/java/org/apache/aries/cdi/weld/BundleResourcesLoader.java
+++ cdi-weld/src/main/java/org/apache/aries/cdi/weld/BundleResourcesLoader.java
@@ -16,11 +16,16 @@ package org.apache.aries.cdi.weld;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.aries.cdi.spi.loader.SpiLoader;
 import org.jboss.weld.resources.spi.ResourceLoader;
@@ -33,6 +38,36 @@ import org.osgi.framework.wiring.BundleWire;
 import org.osgi.framework.wiring.BundleWiring;
 
 public class BundleResourcesLoader implements ProxyServices, ResourceLoader {
+
+	private static java.lang.reflect.Method defineClass1, defineClass2;
+	private static final AtomicBoolean classLoaderMethodsMadeAccessible = new AtomicBoolean(false);
+
+	public static void makeClassLoaderMethodsAccessible() {
+		// the AtomicBoolean make sure this gets invoked only once as WeldStartup is triggered per deployment
+		if (classLoaderMethodsMadeAccessible.compareAndSet(false, true)) {
+			try {
+				AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
+					@Override
+					public Object run() throws Exception {
+						Class<?> cl = Class.forName("java.lang.ClassLoader");
+						final String name = "defineClass";
+
+						defineClass1 = cl.getDeclaredMethod(name, String.class, byte[].class, int.class, int.class);
+						defineClass2 = cl.getDeclaredMethod(name, String.class, byte[].class, int.class, int.class, ProtectionDomain.class);
+						defineClass1.setAccessible(true);
+						defineClass2.setAccessible(true);
+						return null;
+					}
+				});
+			} catch (PrivilegedActionException pae) {
+				throw new RuntimeException("cannot initialize ClassPool", pae.getException());
+			}
+		}
+	}
+
+	static {
+		makeClassLoaderMethodsAccessible();
+	}
 
 	BundleResourcesLoader(SpiLoader loader, Bundle spiImplBundle) {
 		BundleWiring spiImplWiring = spiImplBundle.adapt(BundleWiring.class);
@@ -84,6 +119,34 @@ public class BundleResourcesLoader implements ProxyServices, ResourceLoader {
 	}
 
 	@Override
+	public Class<?> defineClass(Class<?> originalClass, String className, byte[] classBytes, int off, int len) throws ClassFormatError {
+		return defineClass(originalClass, className, classBytes, off, len, null);
+	}
+
+	@Override
+	public Class<?> defineClass(Class<?> originalClass, String className, byte[] classBytes, int off, int len, ProtectionDomain protectionDomain) throws ClassFormatError {
+		try {
+			java.lang.reflect.Method method;
+			Object[] args;
+			if (protectionDomain == null) {
+				method = defineClass1;
+				args = new Object[]{className, classBytes, 0, len};
+			} else {
+				method = defineClass2;
+				args = new Object[]{className, classBytes, 0, len, protectionDomain};
+			}
+			Class<?> clazz = (Class<?>) method.invoke(_classLoader, args);
+			return clazz;
+		} catch (RuntimeException e) {
+			throw e;
+		} catch (java.lang.reflect.InvocationTargetException e) {
+			throw new RuntimeException(e.getTargetException());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
 	public ClassLoader getClassLoader(Class<?> proxiedBeanType) {
 		return _classLoader;
 	}
@@ -106,6 +169,16 @@ public class BundleResourcesLoader implements ProxyServices, ResourceLoader {
 		catch (IOException e) {
 			return Collections.emptyList();
 		}
+	}
+
+	@Override
+	public Class<?> loadClass(Class<?> originalClass, String className) throws ClassNotFoundException {
+		return _classLoader.loadClass(className);
+	}
+
+	@Override
+	public boolean supportsClassDefining() {
+		return true;
 	}
 
 	private static final String ERROR_LOADING_CLASS = "Error loading class ";
